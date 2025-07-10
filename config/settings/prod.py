7a3d0a25fa09ba6a4365b25ff70656d7c87e3e59
olabanjy <shola.albert@gmail.com>
2025-07-10 12:51:55 +0100

    fixes

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -45,7 +45,7 @@ ADMINS = (("Game Splash Support", "hello@zamari.tv"),)
 
 # CELERY related settings
 BROKER_URL = "amqp://localhost"
-CELERY_RESULT_BACKEND = 'amqp://localhost'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
