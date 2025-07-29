ea674c80083510c331f17ba9afeca86d36d5949f
olabanjy <shola.albert@gmail.com>
2025-07-29 17:51:53 +0100

    fixes

--- ums/urls.py
+++ ums/urls.py
@@ -41,6 +41,12 @@ urlpatterns = [
     path("campaign/kmmobi/", kmmobi_campaign_url, name="campaign-kmmobi"),
     path("campaign/mobikok/", mobikok_campaign_url, name="campaign-mobikok"),
 
+    path(
+        "get_cr_data/",
+        get_cr_data,
+        name="get_cr_data",
+    ),
+
     path("check_sub_status/", check_sub_status, name="check_sub_status"),
     path("check_task_result/", check_task_result, name="check_task_result"),
 ]
