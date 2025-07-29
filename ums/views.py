149b16f4c82b56bec97e6c45f1ea46265cb5e01d
olabanjy <shola.albert@gmail.com>
2025-07-29 18:11:32 +0100

    fixes

--- ums/views.py
+++ ums/views.py
@@ -366,7 +366,7 @@ def get_cr_data(request):
     if not partner:
         return JsonResponse({"status": 400, "message": "Partner required"})
 
-    tracks_qs = models.CampaignTracker.objects.filter(
+    tracks_qs = CampaignTracker.objects.filter(
         created_at__date=date_obj,
         provider=partner,
     )
