ee1cf20d16f988aa7681c6c1972c153e25bc4698
olabanjy <shola.albert@gmail.com>
2025-09-08 13:54:04 +0100

    fixes

--- ums/urls.py
+++ ums/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path("campaign/mobikok/", mobikok_campaign_url, name="campaign-mobikok"),
     path("campaign/angel-media/", angel_media_campaign_url, name="campaign-angel-media"),
     path("campaign/tc/", neth_campaign_url, name="campaign-neth"),
+    path("campaign/shine/", shine_campaign_url, name="campaign-shine"),
 
     path(
         "get_cr_data/",
